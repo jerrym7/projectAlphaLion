5d0557a4073317960d02971a94ee488ea28611a7
jerrym7 <diino4l@gmail.com>
2017-10-30 13:29:34 -0700

    AlphaLion
    Added background music

--- ProjectAlphaLion/app/src/main/java/com/example/stude/projectalphalion/Level1.java
+++ ProjectAlphaLion/app/src/main/java/com/example/stude/projectalphalion/Level1.java
@@ -1,15 +1,19 @@
 package com.example.stude.projectalphalion;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+
+import java.util.Random;
 
 /**
  * Created by stude on 10/18/2017.
  */
 
 public class Level1 extends Activity {
+    MediaPlayer bgroundMusic;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -20,5 +24,29 @@ public class Level1 extends Activity {
 
         //full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        startBackgroundMusic();
+    }
+    public void startBackgroundMusic()
+    {
+        int[] sounds={R.raw.sellingdrugs, R.raw.deadpresidents};
+        Random r = new Random();
+        int Low = 0;
+        int High = 2;
+        int rand = r.nextInt(High-Low) + Low;
+        bgroundMusic = MediaPlayer.create(getApplicationContext(),sounds[rand]);
+        bgroundMusic.start();
+        // bgroundMusic = MediaPlayer.create(MainActivity.this,R.raw.sellingdrugs);
+        bgroundMusic.setLooping(true);
+        // bgroundMusic.start();
+    }
+    /*
+        Function to stop the music after it they close the app
+     */
+    protected void onPause()
+    {
+        super.onPause();
+        bgroundMusic.release();
+        finish();
     }
 }
