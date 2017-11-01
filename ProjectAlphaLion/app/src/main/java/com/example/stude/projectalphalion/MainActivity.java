1fc49ab2b29c9767b80a14fc06999dab712800cb
jerrym7 <diino4l@gmail.com>
2017-10-31 17:40:05 -0700

    AlphaLion
    added a main song

--- ProjectAlphaLion/app/src/main/java/com/example/stude/projectalphalion/MainActivity.java
+++ ProjectAlphaLion/app/src/main/java/com/example/stude/projectalphalion/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.stude.projectalphalion;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
-
+    MediaPlayer bgroundMusic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +17,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        startBackgroundMusic();
     }
     public void onButtonClick(View v)
     {
@@ -29,6 +31,21 @@ public class MainActivity extends Activity {
             Intent i = new Intent(MainActivity.this, Settings.class);
             startActivity(i);
         }
+    }
+    public void startBackgroundMusic()
+    {
+        bgroundMusic = MediaPlayer.create(MainActivity.this,R.raw.main_song);
+        bgroundMusic.setLooping(true);
+        bgroundMusic.start();
+    }
+    /*
+       Function to stop the music after it they close the app
+    */
+    protected void onPause()
+    {
+        super.onPause();
+        bgroundMusic.release();
+        finish();
     }
 
 }
