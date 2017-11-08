99a813c9c3f09ed57240cdb129126b827286aaa9
lanceraju <33470233+lanceraju@users.noreply.github.com>
2017-11-07 18:09:24 -0800

    Update Driver.java
    I've commented on the code with detailed explanations for how it operates. Also, an answer list generator has also been added.

--- Driver.java
+++ Driver.java
@@ -1,47 +1,76 @@
 /*
- *  **      Title: Prototype - Level Generator v1.2
+ *  **      Title: Prototype - Level Generator v1.25
  *  ** Programmer: Lance Z Raju
- *  **       Date: 11/07/2017
+ *  **       Date:
  */
 
 import java.util.*;
 public class Driver {
+    //==========================================================================
+    //==========================================================================
+    // ASboundary::int - represents maximum value from [0 to ASBoundary-1) for addition 
+    //                  and subtraction values in a level
+    // MDBoundary::int - same idea as ASBoundary, but for multiplying/dividing.
+    // operation::int - determine operation used; add=1, subtract=2, multiply =3,
+    //                  divide = 4
+    // answer:: int - the answer.
+    //==========================================================================
+    //==========================================================================
     public static int ASboundary = 20;
     public static int MDboundary = 12;
     public static int operation = 0;
     public static int answer = 0;
+    public static Random RNG = new Random();
     public static void main (String[] args){
         
-        boolean winning = true;
+        
+        
+        // Although levelNum is used for displaying, it also decides how big a
+        // problem will be, and also how big the numbers will be.
         double levelNum = 1.1;
         
         
+        //Continue loop while player is winning. Starts off true, because
+        // the player is already winning by choosing our amazing game.
+        boolean winning = true;
         while(winning){
             //Display level with levelNum; increment by 0.1 each loop run.
             System.out.println("Level "+ levelNum );    
             
             
-            
+            //Difficulty mechanic. Level multiples of 0.5 (level 1.5, 2.0, 2.5..)
+            // will have larger boundaries, which means larger numbers potentially
             if (levelNum%0.5==0){
-                ASboundary = ASboundary + 8;
+                ASboundary = ASboundary + 15;
                 MDboundary = MDboundary + 2;
             }
+            
+            // Call generate, which creates an array of unique, random numbers
+            // to be used in a question.
             int question[]= generate(levelNum);
             answer = question[0];
+            
+            //toString will be the output, modified based on operation (+, -, *, /)
             String toString = "" + question[0];
             
+            //Adding 
             if(operation == 1){
+                //Note: each for-loop starts at value 1, because the first value MUST be added
+                // to the 'answer'. Otherwise, you end up subtracting answer (which is 0)
+                // by all the values in the question.
                 for (int i = 1; i < question.length; i++){
                     answer = answer + question[i];
                     toString += " + " + question[i];
                 }
             }
+            //Subtracting
             else if(operation == 2){
                 for (int i = 1; i < question.length; i++){
                     answer = answer - question[i];
                     toString += " - " + question[i]; 
                 }
             }
+            
             else if(operation == 3){
                 for (int i = 1; i < question.length; i++){
                     answer = answer * question[i];
@@ -55,10 +84,25 @@ public class Driver {
                 }
             }
             else{
+                //fail-case. wont occur though
                 System.out.println("If Operation failed.");
             }
+            //display question
             System.out.println(toString);
-            System.out.println("Answer should be :" + answer);
+            // Rusty answer generator, but it does create a unique positioning
+            // system for each answer. Could be improved if deviation formula is
+            // used for 'wrong' answers added to the list.
+            ArrayList <Integer> allAnswers = new ArrayList<Integer>();
+            allAnswers.add(answer);
+            allAnswers.add(answer + 1);
+            allAnswers.add(answer - 1);
+            allAnswers.add(answer +10);
+
+            int pick = 0;
+            while (!allAnswers.isEmpty()){
+                pick = RNG.nextInt(allAnswers.size());
+                System.out.println("\t" + allAnswers.remove(pick));
+            }
             Scanner s = new Scanner(System.in);
             int playerchoice = s.nextInt();
             if (answer == playerchoice){
@@ -69,38 +113,23 @@ public class Driver {
                 winning = false;
             }
             
-            //Call play. If player wins the level, winning continues.
-            //winning = play(ASboundary);
-            
-            
-            
-            
-            
-            //Increment level
             levelNum = levelNum+0.1;
-        }
+        }// end -while loop for game. If winning = false, exits.
         
-        
+        System.out.println("Thanks for playing");
     }
     
-    public static boolean play(int range){
-        
-        
-        
-        
-        
-        
-        return true;
-   
-    
-    }
+  
     public static int[] generate(double levelNum){
+        //As the level increases, the likelihood of a 3 number, 4 number, or 5 number
+        // problem increases. At level 1.1, the likelihood of a 3 number problem is 
+        // '10%'.
         Random rng = new Random();
         int temp = (int)levelNum*10;
         int equationSize = rng.nextInt(temp) +1;
         equationSize = equationSize/10 + 1;
         int x[] = new int[equationSize+1];
-        System.out.println("Equation size : " + equationSize);
+        
         
         //1 = add, 2 = subtract. 3 = mult, 4 = divide
         operation = rng.nextInt(4)+1;
@@ -119,3 +148,4 @@ public class Driver {
         return x;
     }
 }
+
