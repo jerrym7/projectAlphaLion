1819448ae6945a3e8900c0969a0e732f9f19b360
jerrym7 <diino4l@gmail.com>
2017-11-07 20:39:37 -0800

    u

--- ProjectAlphaLion/app/src/main/java/com/example/stude/projectalphalion/Level1.java
+++ ProjectAlphaLion/app/src/main/java/com/example/stude/projectalphalion/Level1.java
@@ -19,6 +19,8 @@ public class Level1 extends Activity {
     int score = 0;
     TextView scoreText,questionText;
     Button p1_button,p2_button,p3_button;
+    int[] answers = new int[3]; //answers
+    boolean[] correct = new boolean[3]; //correct or not
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -31,26 +33,24 @@ public class Level1 extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         scoreText = findViewById(R.id.score);
         questionText = findViewById(R.id.question_text);
+        generateQ();
+        p1_button = (Button)findViewById(R.id.button0);
+        p3_button = (Button)findViewById(R.id.button2);
+        p2_button = (Button)findViewById(R.id.button1);
         //startBackgroundMusic();
 
     }
 
-    /* not yet done//prototype 11/7/17
-     public void question2(View v) {
-        //update text of question and buttons
-        int x[] = new int[3];
-        boolean[] y = new boolean[3];
-        questionText.setText("What is "+foo(x,y) +"?");
-        p1_button = (Button)findViewById(R.id.wronganswer_2_1);
-        p1_button.setText(x[0]);
-        p2_button = (Button)findViewById(R.id.theanswertotwo);
-        p2_button.setText(x[1]);
-        p3_button = (Button)findViewById(R.id.wronganswer_2_2);
-        p3_button.setText(x[2]);
+    private void generateQ() {
 
+        questionText.setText(foo(answers,correct));//call the function
 
+        p1_button.setText(Integer.toString(answers[0]));
+        p2_button.setText(Integer.toString(answers[1]));
+        p3_button.setText(Integer.toString(answers[2]));
     }
-     */
+
+
     /**public void startBackgroundMusic()
     {
         int[] sounds={R.raw.sellingdrugs, R.raw.deadpresidents};
@@ -81,16 +81,8 @@ public class Level1 extends Activity {
         finish();
     }
     public void onClick(View v) {
-        int[] answers = new int[3]; //answers
-        boolean[] correct = new boolean[3]; //correct or not
-        questionText.setText(foo(answers,correct));//call the function
-        p1_button = (Button)findViewById(R.id.button0);
-        p1_button.setText(answers[0]);
-        p2_button = (Button)findViewById(R.id.button1);
-        p2_button.setText(answers[1]);
-        p3_button = (Button)findViewById(R.id.button2);
-        p3_button.setText(answers[2]);
-        if(v.getId() == R.id.button0 )
+
+        if(v == p1_button )
         {
             if(correct[0]==true) {
                 score += 1;
@@ -102,8 +94,9 @@ public class Level1 extends Activity {
                 scoreText.setBackgroundColor(Color.RED);
                 scoreText.setText(Integer.toString(score));
             }
+            generateQ();
         }
-        else if(v.getId() == R.id.button1 )
+        else if(v == p2_button )
         {
             if(correct[1]==true) {
                 score += 1;
@@ -115,6 +108,7 @@ public class Level1 extends Activity {
                 scoreText.setBackgroundColor(Color.RED);
                 scoreText.setText(Integer.toString(score));
             }
+            generateQ();
         }
         else
         {
@@ -128,6 +122,7 @@ public class Level1 extends Activity {
                 scoreText.setBackgroundColor(Color.RED);
                 scoreText.setText(Integer.toString(score));
             }
+            generateQ();
         }
 
     }
@@ -147,6 +142,17 @@ public class Level1 extends Activity {
             else {
                 x[i] = rand.nextInt(11) + rand.nextInt(11);
                 y[i] = false;
+            }
+        }
+        for(int i=0;i<x.length-1;i++)
+        {
+            for(int j = i+1;j<x.length;j++)
+            {
+                if(x[i]==x[j])
+                {
+                    return foo(x,y);
+                }
+
             }
         }
         return num1+" + " +num2;
