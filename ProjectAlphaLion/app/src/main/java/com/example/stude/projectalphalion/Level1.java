42a5adb88844f214a0f0adce8b43e045116e6a04
jerrym7 <diino4l@gmail.com>
2017-12-07 16:11:49 -0800

    Revert "send the level information through an intent"
    This reverts commit 9042028e182c0c3066d0ca54ee8b256a9918332f.

--- ProjectAlphaLion/app/src/main/java/com/example/stude/projectalphalion/Level1.java
+++ ProjectAlphaLion/app/src/main/java/com/example/stude/projectalphalion/Level1.java
@@ -57,7 +57,6 @@ public class Level1 extends Activity {
             h.postDelayed(r1,200);
         }
     };
-
     public Runnable r1 = new Runnable() {
         @Override
         public void run() {
@@ -119,9 +118,8 @@ public class Level1 extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.question_two);
-        Bundle getLevel = getIntent().getExtras();
-        level = getLevel.getDouble("level");
-                //set title off
+
+        //set title off
         hearts=new ImageView[3];
         hearts[0]=(ImageView)findViewById(R.id.imageView0);
         hearts[0].setImageResource(R.drawable.heart);
@@ -130,8 +128,8 @@ public class Level1 extends Activity {
         hearts[2]=(ImageView)findViewById(R.id.imageView2);
         hearts[2].setImageResource(R.drawable.heart);
         //load level and timer
-        level = level;
-        origTIme=level * 0.2 + 10;
+        levelNum = level;
+        origTIme=levelNum * 0.2 + 10;
         timeInSeconds = origTIme;
         timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -277,7 +275,6 @@ public class Level1 extends Activity {
         //Continue loop while player is winning. Starts off true, because
         // the player is already winning by choosing our amazing game.
         boolean winning = true;
-
         if(lives>0){
             //Display level with levelNum; increment by 0.1 each loop run.
 
--- ProjectAlphaLion/app/src/main/java/com/example/stude/projectalphalion/Play.java
+++ ProjectAlphaLion/app/src/main/java/com/example/stude/projectalphalion/Play.java
@@ -24,45 +24,38 @@ public class Play extends Activity {
     {
         if(v.getId() == R.id.lvl1Button )
         {
-            level=0;
+            level=1.0;
             Intent i = new Intent(Play.this, Level1.class);
-            i.putExtra("level", level);//send data to the other class
             startActivity(i);
-
         }
         else if(v.getId() == R.id.lvl2Button )
         {
             level=1.0;
             Intent i = new Intent(Play.this, Level1.class);
-            i.putExtra("level", level);//send data to the other class
             startActivity(i);
         }
         else if(v.getId() == R.id.lvl3Button )
         {
             level=2.0;
             Intent i = new Intent(Play.this, Level1.class);
-            i.putExtra("level", level);//send data to the other class
             startActivity(i);
         }
         else if(v.getId() == R.id.lvl4Button )
         {
             level=3.0;
             Intent i = new Intent(Play.this, Level1.class);
-            i.putExtra("level", level);//send data to the other class
             startActivity(i);
         }
         else if(v.getId() == R.id.lvl5Button )
         {
             level=4.0;
             Intent i = new Intent(Play.this, Level1.class);
-            i.putExtra("level", level);//send data to the other class
             startActivity(i);
         }
         else if(v.getId() == R.id.lvl6Button )
         {
             level=5.0;
             Intent i = new Intent(Play.this, Level1.class);
-            i.putExtra("level", level);//send data to the other class
             startActivity(i);
         }
     }
